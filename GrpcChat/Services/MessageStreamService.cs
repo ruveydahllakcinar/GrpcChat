462c2b7878c69b349c5f55e0210eb41969c38850
Ruveyda Hilal <rhakcinar@gmail.com>
2022-05-27 14:01:44 +0300

    Add project files.

---
+++ GrpcChat/Services/MessageStreamService.cs
@@ -0,0 +1,59 @@
+﻿using Grpc.Core;
+using GrpcChat.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcChat.Services
+{
+    public class MessageStreamService : StreamService.StreamServiceBase
+    {
+
+        private ILogger<MessageStreamService> _logger;
+
+
+        public MessageStreamService(ILogger<MessageStreamService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task StartStreaming(IAsyncStreamReader<StreamMessage> requestStream, IServerStreamWriter<StreamMessage> responseStream, ServerCallContext context)
+        {
+            if (requestStream != null)
+            {
+                if (!await requestStream.MoveNext())
+                    return;
+            }
+
+            try
+            {
+                if (!string.IsNullOrEmpty(requestStream.Current.Message))
+                {
+                    if (string.Equals(requestStream.Current.Message, "qw!", StringComparison.OrdinalIgnoreCase))
+                        return;
+
+                }
+
+                var message = requestStream.Current.Message;
+                Console.WriteLine($"Message from client: {requestStream.Current.Username} Message: {requestStream.Current.Message }");
+                await responseStream.WriteAsync(new StreamMessage
+                {
+                    Username = requestStream.Current.Username,
+                    Message = $"Reply stream from the server @: {DateTime.UtcNow:f} to {requestStream.Current.Username}"
+
+
+                });
+            
+            
+            }
+
+            catch (Exception e)
+            {
+                _logger.LogInformation(e.Message);
+            }
+
+        }
+    }
+}
---
+++ GrpcClient/Program.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcChat.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Press enter to cont....");
+            Console.ReadLine();
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new StreamService.StreamServiceClient(channel);
+            string username = "Rüv";
+            using var stream = client.StartStreaming();
+            var response = Task.Run(async () =>
+            {
+                await foreach (var rm in stream.ResponseStream.ReadAllAsync())
+                    Console.WriteLine(rm.Message);
+            });
+
+            Console.WriteLine("Enter message to stream to server");
+            while (true)
+            {
+                var text = Console.ReadLine();
+                if (string.IsNullOrEmpty(text))
+                    break;
+                await stream.RequestStream.WriteAsync(new StreamMessage
+                {
+                    Username = username,
+                    Message = text
+                });
+
+            }
+            Console.WriteLine("Disconnectioning........");
+            await stream.RequestStream.CompleteAsync();
+            await response;
+        }
+    }
+}
